320ad0d8229408d25d6cf64a5ae40dca5daa74f1
wbs306 <wbs306@gmail.com>
2022-07-31 12:00:05 +0800

    Galaga: Add symlink for mdota
    Change-Id: I38b39379f450c75f35598e6e43054aa5ad0f3e78

--- device.mk
+++ device.mk
@@ -179,6 +179,9 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Radio
+PRODUCT_PACKAGES += \
+    vendor_mdota_symlink
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
