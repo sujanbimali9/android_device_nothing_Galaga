23bb74f8b662a5051058ae221498b332f8142d1d
bengris32 <bengris32@protonmail.ch>
2024-02-23 14:25:45 +0000

    Galaga: Stop vtservice and neural networks starting in charger
    * The MTK Neural Networks service is started via a property trigger.
      This HAL starts a lot earlier than other HALs since it is started
      with "start" instead of "enable", the difference between them being
      that "start" will immediately cause the service to try and start, while
      "enable" effectively removes the "disabled" attribute from the service
      (so in the case of a HAL, it will be started with all the other services
      in class hal instead of immediately).
    
    * This initially didn't have any side effects until I noticed the Neural Networks
      service constantly crashing in offline charger mode. To fix this, simply
      convert the "start" command to an "enable" to ensure that this service is not
      only started at the right time (in class hal), but also only started when
      needed (and not in offline charger mode).
    
    * Do the same for vtservice as well.
    
    Change-Id: Iacf310fb2211117aaf028ad6bf0814a7d447cf53
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- extract-files.py
+++ extract-files.py
@@ -33,6 +33,11 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    (
+        'system_ext/etc/init/init.vtservice.rc',
+        'vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc'
+    ): blob_fixup()
+        .regex_replace('start', 'enable'),
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
     'vendor/bin/hw/android.hardware.security.keymint@3.0-service.trustonic': blob_fixup()
