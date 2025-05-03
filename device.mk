50959924e2536481ff045a0472cf703e17bdc9e1
claxten10 <claxten10@gmail.com>
2025-05-03 16:10:12 +0200

    Galaga: overlay: Configure inital brightness values
    Change-Id: I27a411d2bd1b58d65634adb7c96d8d70001a7ccf
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -173,6 +173,9 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+PRODUCT_PACKAGES += \
+    FrameworkResOverlayGalaga
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr \
