296d60e64e95637e873bfd8ca19e54c796832063
rexix01 <sebidk700@gmail.com>
2025-07-02 06:40:30 +0000

    Galaga: Include common MediaTek FCM
    Change-Id: If526a75b4a3c7b1e1f5576200ac248e7d2e027c8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,7 +171,8 @@ BOARD_AVB_VENDOR_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 # VINTF
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/vintf/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 
 # WiFi
 WPA_SUPPLICANT_VERSION := VER_0_8_X
