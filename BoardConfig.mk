97aafdf364bbbcfbe0a2ecac43899ad507e4fc15
rexix01 <sebidk700@gmail.com>
2025-11-26 16:53:19 -0500

    Galaga: Define Arch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,11 @@
 
 DEVICE_PATH := device/nothing/Galaga
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a76
+
 # Inherit the proprietary files
 include vendor/nothing/Galaga/BoardConfigVendor.mk
