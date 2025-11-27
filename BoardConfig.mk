479a30e64ee90d96901c0a1ba330c5efdf12bbb0
rexix01 <sebidk700@gmail.com>
2025-11-27 13:08:10 -0500

    Galaga: Import device matrix
    Change-Id: Ia7f656518928a63e0479bc9806f1ce8334e99261

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,7 @@ BOARD_AVB_VENDOR_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 
 # VINTF
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/vintf/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     vendor/lineage/config/device_framework_matrix.xml \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
