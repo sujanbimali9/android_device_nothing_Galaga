9af57b888d4714b81efcc98e3686587b3e6abc25
rexix01 <sebidk700@gmail.com>
2025-12-03 13:40:53 -0500

    fixup! Galaga: Configure audio stack

--- extract-files.py
+++ extract-files.py
@@ -55,6 +55,11 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/mt6878/libmtkcam_taskmgr.so',
      'vendor/lib64/hw/hwcomposer.mtk_common.so'): blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
+    'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so')
+        .replace_needed('libalsautils.so', 'libalsautils-stock.so')
+        .binary_regex_replace(b'A2dpsuspendonly', b'A2dpSuspended\x00\x00')
+        .binary_regex_replace(b'BTAudiosuspend', b'A2dpSuspended\x00'),
      'vendor/lib64/vendor.mediatek.hardware.bluetooth.audio-V1-ndk.so': blob_fixup()
         .replace_needed('android.hardware.audio.common-V1-ndk.so', 'android.hardware.audio.common-V2-ndk.so'),
      'vendor/lib64/mt6878/libpqconfig.so': blob_fixup()
