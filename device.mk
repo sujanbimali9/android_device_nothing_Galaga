e0ac199b2b2a24e96e712932a7d9e4e8986edb3c
Cosmin Tanislav <demonsingur@gmail.com>
2026-02-02 19:29:16 +0530

    Galaga: Add support of udfps long press sensor
    Signed-off-by: sm455 <221133194+sm455@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -235,7 +235,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
-    android.hardware.sensors@2.0-subhal-impl-1.0
+    android.hardware.sensors@2.0-subhal-impl-1.0 \
+    sensors.nothing
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
