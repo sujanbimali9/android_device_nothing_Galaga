eb5426738c09877934f7ff5c8cf81a5306fa1eb0
Mashopy <eliasgheeraert@gmail.com>
2026-04-07 10:57:08 +0200

    Galaga: Rename memtrack service to the new impl

--- device.mk
+++ device.mk
@@ -114,7 +114,7 @@ PRODUCT_PACKAGES += \
 
 # Graphics
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack-service.mediatek-mali
+    android.hardware.memtrack-service.mediatek
 
 PRODUCT_PACKAGES += \
     ANGLE
