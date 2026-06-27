618fe65a26bd6b715c890679813cd4862f5ccd45
Frost <frost.github@proton.me>
2026-06-27 11:48:04 +0000

    Galaga: Rename libmnl to libmnl-v33
    Signed-off-by: Andreyka445 <seleznevandrey186@outlook.com>

--- extract-files.py
+++ extract-files.py
@@ -119,6 +119,8 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libsilkybrightnesscore.so',
     ): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    'vendor/bin/mnld': blob_fixup()
+        .replace_needed('libmnl.so', 'libmnl-v33.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
