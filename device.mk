287218a199487fec1c8e25fe3b3010b69b131ba7
Andreyka445 <seleznevandrey186@outlook.com>
2026-06-29 13:47:00 +0000

    Galaga: Fix libsink.so
    - Add an AudioTrack constructor shim for the A16 libsink-mtk blob so libsink
      links and works against the A17 constructor.
    
    Signed-off-by: Andreyka445 <seleznevandrey186@outlook.com>

--- device.mk
+++ device.mk
@@ -72,6 +72,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 
+PRODUCT_PACKAGES += \
+    libaudiotrack_shim
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth-service.mediatek
